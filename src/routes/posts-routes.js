9e64cef3b5a4a629b7a6f2c8b72f6ceaf7b81464
unknown <alexenache43@yahoo.com>
2021-03-22 13:03:51 +0200

    set-object-on-single-post

--- src/routes/posts-routes.js
+++ src/routes/posts-routes.js
@@ -72,15 +72,17 @@ module.exports = (app) => {
             if(!doc) return res.status(404).send('This post does not exist');
 
             res.status(200).json({
-                postId: doc._id,
-                username: doc.username,
-                title: doc.title,
-                text: doc.text,
-                location: doc.location,
-                category: doc.category,
-                postImage: doc.postImage,
-                likes: doc.likes,
-                shares: doc.shares,
+                'singlePost' : {
+                    postId: doc._id,
+                    username: doc.username,
+                    title: doc.title,
+                    text: doc.text,
+                    location: doc.location,
+                    category: doc.category,
+                    postImage: doc.postImage,
+                    likes: doc.likes,
+                    shares: doc.shares,
+                }
             });
         } catch (error) {
             res.status(500).send('Something went wrong!');
