53512075f231aba966ae1543dd2bdcb446a0e985
unknown <alexenache43@yahoo.com>
2021-03-30 10:18:58 +0300

    make-routes-file-for-replies

--- src/routes/comments-routes.js
+++ src/routes/comments-routes.js
@@ -1,5 +1,5 @@
 const serverConfig = require('../config/config')
-const { Comments, Replies } = require('../models')
+const { Comments } = require('../models')
 const mongoose = require('mongoose')
 const cors = require('cors')
 const auth = require('../utils/auth-utils');
@@ -85,33 +85,5 @@ module.exports = (app) => {
         }
     });
 
-    app.put(`${serverConfig.BASE_URL}/createReply`, cors(), auth.validateToken, async (req, res) => {
-        try {
 
-            if(!req.body.text || !req.body.commentId) {
-                return res.status(400).send('Data is not provided correctly');
-            }
-            const commentId = mongoose.Types.ObjectId(req.body.commentId);
-            const userId = mongoose.Types.ObjectId(req.user);
-    
-            const reply = new Replies({
-                text: req.body.text,
-                commentId: commentId,
-                userId: userId,
-                createdDate: Date.now(),
-                likes: [],
-            });
-
-            const doc = await reply.save();
-
-            return res.status(200).json({
-                success: true,
-                replyId: doc._id,
-                msg: 'Reply created successfully'
-            });
-        } catch (error) {
-            console.log(error)
-            return res.status(500).send('Something went wrong!');
-        }
-    });
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,12 @@ const authRoutes = require('./auth-routes');
 const commentsRoutes = require('./comments-routes');
 const likesRoutes = require('./likes-routes');
 const postsRoutes = require('./posts-routes');
+const repliesRoutes = require('./replies-routes');
 
 module.exports = (app) => {
     authRoutes(app);
     postsRoutes(app);
     likesRoutes(app);
     commentsRoutes(app);
+    repliesRoutes(app);
 }
