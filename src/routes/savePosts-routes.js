747535dc86ffab8d12d7fb50c86d929a75a968b8
unknown <alexenache43@yahoo.com>
2021-06-02 10:20:58 +0300

    delete-saved-posts-endpoint

--- src/routes/savePosts-routes.js
+++ src/routes/savePosts-routes.js
@@ -26,6 +26,7 @@ module.exports = (app) => {
 
             const postId = mongoose.Types.ObjectId(req.body.postId);
             const userId = mongoose.Types.ObjectId(req.user);
+
             Account.findByIdAndUpdate(userId, {
                 $push: { "savedPosts": postId },
                 $inc: { "totalSavedPosts": 1 }
@@ -33,7 +34,43 @@ module.exports = (app) => {
                 if (err) return res.status(404).send('Post not saved');
                 if (result) return res.status(200).json({
                     success: true,
-                    msg: "Post changed successfully"
+                    msg: "Post saved successfully"
+                });
+            });
+        } catch (error) {
+            console.log(error)
+            res.status(500).send('Something went wrong!');
+        }
+    });
+
+    /**
+    /api/deleteSavedPost
+    req.body: 
+        postId: String
+
+    validateToken:
+        user: String
+
+    res:
+        success: true
+    */
+    app.put(`${serverConfig.BASE_URL}/deleteSavedPost`, cors(), auth.validateToken, async (req, res) => {
+        try {
+            if (!req.body.postId) {
+                return res.status(400).send('Post id not provided correctly!');
+            }
+
+            const postId = mongoose.Types.ObjectId(req.body.postId);
+            const userId = mongoose.Types.ObjectId(req.user);
+
+            Account.findByIdAndUpdate(userId, {
+                $pull: { "savedPosts": postId },
+                $inc: { "totalSavedPosts": -1 }
+            }, (err, result) => {
+                if (err) return res.status(404).send('Post not unsaved');
+                if (result) return res.status(200).json({
+                    success: true,
+                    msg: "Post unsaved successfully"
                 });
             });
         } catch (error) {
