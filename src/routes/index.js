31c8b00e2f30b2c62c5fd4b6173d846c6f76d231
unknown <alexenache43@yahoo.com>
2021-06-02 12:53:03 +0300

    rename

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,7 @@ const commentsRoutes = require('./comments-routes');
 const likesRoutes = require('./likes-routes');
 const postsRoutes = require('./posts-routes');
 const repliesRoutes = require('./replies-routes');
-const savePosts = require('./savePosts-routes');
+const savePostsRoutes = require('./savePosts-routes');
 
 module.exports = (app) => {
     authRoutes(app);
@@ -13,5 +13,5 @@ module.exports = (app) => {
     likesRoutes(app);
     commentsRoutes(app);
     repliesRoutes(app);
-    savePosts(app);
+    savePostsRoutes(app);
 }
